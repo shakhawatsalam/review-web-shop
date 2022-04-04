3ee825e4a87ebdf6473ecaada85274e5b053e856
skshawon666 <75742736+skshawon666@users.noreply.github.com>
2022-04-04 11:02:19 +0600

    chart is done

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -42,9 +42,9 @@ const Dashboard = () => {
     ];
     return (
         <div className='max-w-7xl  mx-auto'>
-            <div className='grid gap-2 grid-cols-2'>
+            <div className='grid gap-4 grid-cols-2'>
                 <div className="linechart">
-                    <h1>hello world</h1>
+                    <h1 className='text-center text-blue-700 font-bold mb-3'>Investment VS Revenue</h1>
                     <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
                         <Line type="monotone" dataKey="investment" stroke="#8884d8" />
                         <Line type="monotone" dataKey="revenue" stroke="#8884cd" />
@@ -57,7 +57,7 @@ const Dashboard = () => {
                     </LineChart>
                 </div>
                 <div className="areaChart">
-                        <h1>hello world</h1>
+                    <h1 className='text-center text-blue-700 font-bold mb-3'>Investment VS Revenue</h1>
                     <AreaChart width={600} height={300} data={data}
                         margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
                         <defs>
@@ -78,23 +78,27 @@ const Dashboard = () => {
                         <Area type="monotone" dataKey="revenue" stroke="#82ca9d" fillOpacity={1} fill="url(#colorPv)" />
                     </AreaChart>
                 </div>
-                <div className=''></div>
-                <BarChart width={600} height={300} data={data}>
-                    <h1>hello world</h1>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="month" />
-                    <YAxis />
-                    <Tooltip />
-                    <Legend />
-                    <Bar dataKey="investment" fill="#8884d8" />
-                    <Bar dataKey="revenue" fill="#82ca9d" />
-                </BarChart>
-
-                <PieChart width={730} height={250}>
-                    <Pie data={data} dataKey="investment" nameKey="month" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
-                    <Pie data={data} dataKey="revenue" nameKey="month" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
-                    <Tooltip />
-                </PieChart>
+                <div className='barChart'>
+                    <h1 className='text-center text-blue-700 font-bold mb-3'>Investment VS Revenue</h1>
+                    <BarChart width={600} height={300} data={data}>
+                        <h1>hello world</h1>
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="month" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="investment" fill="#8884d8" />
+                        <Bar dataKey="revenue" fill="#82ca9d" />
+                    </BarChart>
+                </div>
+                <div className="pieChart">
+                    <h1 className='text-center text-blue-700 font-bold mb-3'>Investment VS Revenue</h1>
+                    <PieChart width={730} height={250}>
+                        <Pie data={data} dataKey="investment" nameKey="month" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+                        <Pie data={data} dataKey="revenue" nameKey="month" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+                        <Tooltip />
+                    </PieChart>
+                </div>
             </div>
 
         </div>
