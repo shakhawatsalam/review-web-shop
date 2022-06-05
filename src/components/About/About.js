1880b79e1a67b22d63db693d6ffb3ef33294d8a3
shakhawatsalam <shakhawatsalam726@gmail.com>
2022-06-05 12:27:58 +0600

    privet to public

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1 className='text-center text-4xl font-extrabold'>Ramadan mubarak!!!!</h1>
+            <h1 className='text-center text-4xl font-extrabold'>Ramadan mubarak!! </h1>
         </div>
     );
 };
